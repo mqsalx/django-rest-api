0407fbc18be3c1f617b268a7b7d3a3b6d976fb8e
mqsalx <mqsalx@gmail.com>
2023-07-12 07:43:57 -0300

    Updated files

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-
-from . import views
+from recipes import views
 
 app_name = 'recipes'
 
@@ -44,5 +43,10 @@ urlpatterns = [
         'recipes/theory/',
         views.theory,
         name='theory',
+    ),
+    path(
+        'recipes/api/v2/',
+        views.recipe_api_list,
+        name='recipes_api_v2'
     )
 ]
